fae36e07f76e56a6b44d9cdf40d464db99dea42d
Flora Su <fflorasu@gmail.com>
2025-06-25 21:48:54 -0700

    included build

--- src/components/scenes/Ceiling.tsx
+++ src/components/scenes/Ceiling.tsx
@@ -1,6 +1,6 @@
 import "../../css/ceiling.css"
 import type React from "react"
-import { use, useRef, useState, useEffect } from "react"
+import { use, useRef, useState } from "react"
 //components
 import MainDirectionButton from "../mainGame/MainDirectionButton"
 import CeilingGear from "../subscenes/CeilingGear"
